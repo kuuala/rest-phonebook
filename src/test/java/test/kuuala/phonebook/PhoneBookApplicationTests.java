1996895f7f38c2584c6bc10e024d893da2e4edb6
kuuala <kumaritof@ya.ru>
2019-12-04 13:40:56 +0300

    init

---
+++ src/test/java/test/kuuala/phonebook/PhoneBookApplicationTests.java
@@ -0,0 +1,13 @@
+package test.kuuala.phonebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
